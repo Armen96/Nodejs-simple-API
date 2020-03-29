1053f818f052e94e98d59eda1f846537094bde9e
Armen96 <barsegyan96armen@gmail.com>
2020-03-29 22:03:48 +0400

    Init socket.io

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const http = require('http').Server(app);
 const middlewarePassport = require('./src/app/middlewares/passport');
 import router from './src/routes';
-
+const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 4205;
 
@@ -20,6 +20,19 @@ middlewarePassport(passport);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
+
+
+const io = socketIO(http);
+io.on('connection', (socket) => {
+    socket.on('new-message', (message) => {
+        io.emit('new-message', message);
+    });
+
+    socket.on('messages', () => {
+        io.emit('messages', [{created_at: "26/04/2020", message: "Chis", user_id: "aasdasdasd"}]);
+    });
+});
+
 
 app.use('/api', router);
 
